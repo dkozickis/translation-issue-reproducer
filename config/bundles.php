37bee80d1721f0813156d9070218d8963902b9b4
Deniss Kozickis <deniss.kozickis@gmail.com>
2018-01-31 13:10:27 +0400

    Minimal veeeeeeery minimal

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Translation\PlatformAdapter\Loco\Bridge\Symfony\TranslationAdapterLocoBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
